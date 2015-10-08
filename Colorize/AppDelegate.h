4aac0e85223f8e8646f907cf3b684317ec2bd3a7
fabiogeneral <fabiogeneral@gmail.com>
2015-10-08 17:32:24 -0300

    Initial Commit

---
+++ Colorize/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Colorize
+//
+//  Created by Fabio General on 10/8/15.
+//  Copyright © 2015 Fabio General. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Colorize/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Colorize
+//
+//  Created by Fabio General on 10/8/15.
+//  Copyright © 2015 Fabio General. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
